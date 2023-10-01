3b8ab227e71bde6880ca319324c714acf5d08f29
jaroslav2324 <jaro_r@mail.ru>
2023-10-01 11:31:58 +0300

    aaded radius for tower

---
+++ scenes/tower.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+
+@export var attack_radius = 256
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	input_pickable = true
+	var radius_scale: Vector2 = Vector2(attack_radius / 256.0, attack_radius / 256.0)
+	print("rad scale ", radius_scale)
+	$radiusSprite.apply_scale(radius_scale)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
+
+
+func _on_mouse_entered():
+	$radiusSprite.show()
+	# print("Mouse enntered")
+	pass # Replace with function body.
+
+
+func _on_mouse_exited():
+	$radiusSprite.hide()
+	pass # Replace with function body.
---
+++ scenes/tower_magic.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+
+@export var attack_radius = 25
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
