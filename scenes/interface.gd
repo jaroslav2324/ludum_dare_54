111f73c0f0ad4aba0e3762456ecb71ee7b97374d
Gleb <Gleb@LAPTOP-INM9DLK4>
2023-10-02 11:22:14 +0300

    interface
    the user-friendly interface has become more convenient

--- scenes/interface.gd
+++ scenes/interface.gd
@@ -16,6 +16,15 @@ func _ready():
 func _process(delta):
 	$Panel/coinCount.text = str(Coins)
 	$Panel/ProgressBar.value = Manna
+	if Input.is_action_just_pressed("mT"):
+		_on_magic_tower_button_down()
+		$Panel/magicTower.button_pressed = not $Panel/magicTower.button_pressed
+	if Input.is_action_just_pressed("min"):
+		_on_mine_button_down()
+		$Panel/mine.button_pressed = not $Panel/mine.button_pressed
+	if Input.is_action_just_pressed("rT"):
+		_on_real_towe_button_down()
+		$Panel/realTowe.button_pressed = not $Panel/realTowe.button_pressed
 	pass
 
 func _on_game_currency(coins, manna):
@@ -24,20 +33,33 @@ func _on_game_currency(coins, manna):
 
 
 func _on_mine_toggled(button_pressed):
+	print("asdasd")
 	emit_signal("mineToggled", button_pressed)
-	$Panel/magicTower.button_pressed = false
-	$Panel/realTowe.button_pressed = false
+
 
 
 
 func _on_magic_tower_toggled(button_pressed):
 	emit_signal("magicTowerToggled", button_pressed)
-	$Panel/realTowe.button_pressed = false
-	$Panel/mine.button_pressed = false
+
 	
 
 func _on_real_towe_toggled(button_pressed):
 	emit_signal("towerToggled", button_pressed)
+
+	
+
+
+func _on_magic_tower_button_down():
+	$Panel/realTowe.button_pressed = false
+	$Panel/mine.button_pressed = false
+
+
+func _on_mine_button_down():
+	$Panel/magicTower.button_pressed = false
+	$Panel/realTowe.button_pressed = false
+
+
+func _on_real_towe_button_down():
 	$Panel/magicTower.button_pressed = false
 	$Panel/mine.button_pressed = false
-	
